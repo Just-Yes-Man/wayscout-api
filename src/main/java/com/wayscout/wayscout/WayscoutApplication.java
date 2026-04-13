9621dc28816595426e7be7888e377b189f710060
Just-Yes-Man <yesman@yesman.localdomain>
2026-04-13 16:56:08 -0600

    creacion

---
+++ src/main/java/com/wayscout/wayscout/WayscoutApplication.java
@@ -0,0 +1,13 @@
+package com.wayscout.wayscout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayscoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WayscoutApplication.class, args);
+	}
+
+}
